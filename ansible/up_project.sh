4f613b268ec171b4c6f88ac9d9bba7122c97152d
Olena Zamkova <olena.zamkova@gmail.com>
2019-04-22 20:44:20 +0300

    Ansible settings

---
+++ ansible/up_project.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose.production.yml build
+docker-compose -f docker-compose.production.yml run --rm rails rake db:create
+docker-compose -f docker-compose.production.yml run --rm rails rake db:migrate
+docker-compose -f docker-compose.production.yml stop
+docker-compose -f docker-compose.production.yml up -d
+
+docker ps
